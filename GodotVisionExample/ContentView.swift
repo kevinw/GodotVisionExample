d7999e1addf24e28892597f25a2ffc1391d86883
Adam Watters <adamwatters11@gmail.com>
2024-02-16 17:17:07 -0800

    now in sync with GodotVision 59e4d8c1387190dd87b7e89ab2237e27c4fec923

--- GodotVisionExample/ContentView.swift
+++ GodotVisionExample/ContentView.swift
@@ -8,22 +8,27 @@ struct ContentView: View {
     @StateObject private var coordinator = GodotVisionCoordinator()
     
     var body: some View {
-        VStack {
+        GeometryReader3D { (geometry: GeometryProxy3D) in
             RealityView { content, attachments in
                 // Initialize Godot
                 let rkEntityGodotRoot = coordinator.setupRealityKitScene(content, volumeSize: VOLUME_SIZE)
                 print("All Godot nodes going into \(rkEntityGodotRoot)")
-                
                 if let uiPanel = attachments.entity(for: "ui_panel") {
                     content.add(uiPanel)
-                    
-                    // TODO: better if we can base this on the height of the panel
                     uiPanel.position = .init(0, Float(VOLUME_SIZE.y / -2 + 0.1), Float(VOLUME_SIZE.z / 2 - 0.01))
                 }
-            } update: { _, _ in
-                // Called when SwiftUI @State in this ContentView changes. See docs for RealityView.
+            } update: { content, attachments in
+                // update called when SwiftUI @State in this ContentView changes. See docs for RealityView.
+                // user can change the volume size from the default by selecting a different zoom level.
+                // we watch for changes via the GeometryReader and scale the godot root accordingly
+                let frame = content.convert(geometry.frame(in: .local), from: .local, to: .scene)
+                let volumeSize = simd_double3(frame.max - frame.min)
+                coordinator.changeScaleIfVolumeSizeChanged(volumeSize)
+                if let uiPanel = attachments.entity(for: "ui_panel") {
+                    // swiftui attachment also needs to be repositioned
+                    uiPanel.position = .init(0, Float(volumeSize.y / -2 + 0.1), Float(volumeSize.z / 2 - 0.01))
+                }
             } attachments: {
-                // Add SwiftUI attachments that live in the RealityKit scene.
                 Attachment(id: "ui_panel") {
                     HStack {
                         Button {
