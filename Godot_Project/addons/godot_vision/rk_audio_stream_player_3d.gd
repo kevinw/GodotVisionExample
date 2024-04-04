4333dfb33984e8fced85c467a17d463c6cd83722
Kevin Watters <kevinwatters@gmail.com>
2024-04-04 15:51:04 +0200

    Add auto_prepare_resource for sounds and bump GodotVision version.

--- Godot_Project/addons/godot_vision/rk_audio_stream_player_3d.gd
+++ Godot_Project/addons/godot_vision/rk_audio_stream_player_3d.gd
@@ -2,6 +2,13 @@ class_name RKAudioStreamPlayer3D
 extends AudioStreamPlayer3D
 
 signal on_play(node: AudioStreamPlayer3D)
+signal on_prepare(node: AudioStreamPlayer3D)
+
+@export var auto_prepare_resource: bool = true
+
+func _enter_tree():
+	if auto_prepare_resource:
+		on_prepare.emit(self)
 
 func play_rk(from_position: float = 0.0):
 	on_play.emit(self)
