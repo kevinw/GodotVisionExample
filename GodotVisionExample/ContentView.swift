51a3edfcc71cd3eb2e229440b9afb110d431921e
Kevin Watters <kevinwatters@gmail.com>
2024-04-12 13:47:02 +0200

    Document clearly how to choose the project root.

--- GodotVisionExample/ContentView.swift
+++ GodotVisionExample/ContentView.swift
@@ -5,14 +5,21 @@ import RealityKit
 import GodotVision
 
 struct ContentView: View {
-    @StateObject private var coordinator = GodotVisionCoordinator()
+    @StateObject private var godotVision = GodotVisionCoordinator()
     
     var body: some View {
         GeometryReader3D { (geometry: GeometryProxy3D) in
             RealityView { content, attachments in
+                
+                let pathToGodotProject = "Godot_Project" // The path to the folder containing the "project.godot" you wish Godot to load.
+                
                 // Initialize Godot
-                let rkEntityGodotRoot = coordinator.setupRealityKitScene(content, volumeSize: VOLUME_SIZE)
-                print("All Godot nodes going into \(rkEntityGodotRoot)")
+                let rkEntityGodotRoot = godotVision.setupRealityKitScene(content,
+                                                                         volumeSize: VOLUME_SIZE,
+                                                                         projectFileDir: pathToGodotProject)
+                
+                print("Godot scene root: \(rkEntityGodotRoot)")
+                
                 if let uiPanel = attachments.entity(for: "ui_panel") {
                     content.add(uiPanel)
                     uiPanel.position = .init(0, Float(VOLUME_SIZE.y / -2 + 0.1), Float(VOLUME_SIZE.z / 2 - 0.01))
@@ -23,7 +30,7 @@ struct ContentView: View {
                 // we watch for changes via the GeometryReader and scale the godot root accordingly
                 let frame = content.convert(geometry.frame(in: .local), from: .local, to: .scene)
                 let volumeSize = simd_double3(frame.max - frame.min)
-                coordinator.changeScaleIfVolumeSizeChanged(volumeSize)
+                godotVision.changeScaleIfVolumeSizeChanged(volumeSize)
                 if let uiPanel = attachments.entity(for: "ui_panel") {
                     // swiftui attachment also needs to be repositioned
                     uiPanel.position = .init(0, Float(volumeSize.y / -2 + 0.1), Float(volumeSize.z / 2 - 0.01))
@@ -32,31 +39,31 @@ struct ContentView: View {
                 Attachment(id: "ui_panel") {
                     HStack {
                         Button {
-                            coordinator.reloadScene()
+                            godotVision.reloadScene()
                         } label: {
                             Text("Reload Scene")
                         }
                         
                         Button {
-                            coordinator.changeSceneToFile(atResourcePath: "res://examples/hello/example_godot_vision_scene.tscn")
+                            godotVision.changeSceneToFile(atResourcePath: "res://examples/hello/example_godot_vision_scene.tscn")
                         } label: {
                             Text("Hello")
                         }
                         
                         Button {
-                            coordinator.changeSceneToFile(atResourcePath: "res://examples/physics_toy/physics_toy.tscn")
+                            godotVision.changeSceneToFile(atResourcePath: "res://examples/physics_toy/physics_toy.tscn")
                         } label: {
                             Text("Physics")
                         }
                         
                         Button {
-                            coordinator.changeSceneToFile(atResourcePath: "res://examples/materials/materials.tscn")
+                            godotVision.changeSceneToFile(atResourcePath: "res://examples/materials/materials.tscn")
                         } label: {
                             Text("Materials")
                         }
                         
                         Button {
-                            coordinator.changeSceneToFile(atResourcePath: "res://examples/rigged_models/example_rigged_models.tscn")
+                            godotVision.changeSceneToFile(atResourcePath: "res://examples/rigged_models/example_rigged_models.tscn")
                         } label: {
                             Text("Rigged Models")
                         }
@@ -64,7 +71,7 @@ struct ContentView: View {
                 }
             }
         }
-        .modifier(GodotVisionRealityViewModifier(coordinator: coordinator))
+        .modifier(GodotVisionRealityViewModifier(coordinator: godotVision))
     }
 }
 
