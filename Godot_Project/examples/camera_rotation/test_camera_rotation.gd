f143ce15986a972a21b789cf7f9907dea25c57df
Kevin Watters <kevinwatters@gmail.com>
2024-04-18 17:09:16 +0200

    Add some scaling to the camera rotation test.

--- Godot_Project/examples/camera_rotation/test_camera_rotation.gd
+++ Godot_Project/examples/camera_rotation/test_camera_rotation.gd
@@ -2,26 +2,37 @@ extends Node3D
 
 @onready var cam_node: Node3D = $VisionVolumeCamera
 
+	
+const pos_changes := [Vector3(0, 0, 1), Vector3(1, 0, 0), Vector3(0, 0, -1), Vector3(-1, 0, 0)]
 
 func _ready():
-	await get_tree().create_timer(1.0).timeout
+	await timeout(1)
+	
 	var rot: Vector3 = Vector3(0, 0, 0)
 	var pos: Vector3 = Vector3(0, 0, 0)
 	
-	var pos_changes := [Vector3(0, 0, 1), Vector3(1, 0, 0), Vector3(0, 0, -1), Vector3(-1, 0, 0)]
 	var pos_change_index := 0
 	
 	while true:
-		rot += Vector3(0, 0.5 * PI, 0)
+		await tween_prop(cam_node, "scale", Vector3(1, 1, 1) * 0.3, 0.5)
+		await timeout(0.6)
+		await tween_prop(cam_node, "scale", Vector3(1, 1, 1) * 1.0, 0.5)
+		await timeout(0.6)
 		
+		rot += Vector3(0, 0.5 * PI, 0)
 		pos += pos_changes[pos_change_index] * 4
 		pos_change_index = (pos_change_index + 1) % pos_changes.size()
 		
-		await create_tween().tween_property(cam_node, "rotation", rot, 0.5).set_ease(Tween.EASE_IN_OUT).finished
-		await get_tree().create_timer(0.5).timeout
-		await create_tween().tween_property(cam_node, "position", pos, 0.5).set_ease(Tween.EASE_OUT_IN).finished
-		await get_tree().create_timer(1.0).timeout
+		await tween_prop(cam_node, "rotation", rot, 0.5)
+		await timeout(0.5)
+		await tween_prop(cam_node, "position", pos, 0.5)
+		await timeout(0.5)
 		
+
+func timeout(amt: float):
+	await get_tree().create_timer(amt).timeout
 	
+func tween_prop(node: Node3D, prop_name: String, value, duration: float):
+	await create_tween().tween_property(node, prop_name, value, duration).set_ease(Tween.EASE_IN_OUT).finished
 
 
