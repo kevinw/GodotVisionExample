281028aa45738967afcdabefcfafa645bbcbd167
Kevin Watters <kevinwatters@gmail.com>
2024-04-22 07:57:11 +0200

    Respawn the godot character in the hello example

--- Godot_Project/examples/hello/scripts/tower_enemy.gd
+++ Godot_Project/examples/hello/scripts/tower_enemy.gd
@@ -2,5 +2,14 @@ extends RigidBody3D
 
 @onready var anim_player = $character_meshes/AnimationPlayer
 
+@onready var initial_global_position: Vector3 = self.global_position
+
 func _ready():
 	anim_player.play("walk")
+
+func _process(_delta: float):
+	# respawn the character if it gets knocked off screen
+	if global_position.length() > 40:
+		global_position = initial_global_position + Vector3(0, 7, 0)
+		linear_velocity = Vector3.ZERO
+		angular_velocity = Vector3.ZERO
